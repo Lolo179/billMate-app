d3c8ffe6c0cd4d61e9d2cf6fa37dc76ec2b975bc
Luis Alvarado Dev <luismanolo92@gmail.com>
2026-02-22 22:57:44 +0100

    chore: actualiza proyecto a Java 21 y limpia archivos obsoletos

--- scripts/install-all.bat
+++ scripts/install-all.bat
@@ -1,4 +1,4 @@
-start cmd /k "cd ../billing-service && mvn clean install"
-start cmd /k "cd ../auth-service && mvn clean install"
-start cmd /k "cd ../api-gateway && mvn clean install"
-start cmd /k "cd ../frontend-service && mvn clean install"
+start cmd /k "set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.10.7-hotspot && set PATH=%JAVA_HOME%\bin;%PATH% && cd ../billing-service && mvn clean install"
+start cmd /k "set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.10.7-hotspot && set PATH=%JAVA_HOME%\bin;%PATH% && cd ../auth-service && mvn clean install"
+start cmd /k "set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.10.7-hotspot && set PATH=%JAVA_HOME%\bin;%PATH% && cd ../api-gateway && mvn clean install"
+start cmd /k "set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.10.7-hotspot && set PATH=%JAVA_HOME%\bin;%PATH% && cd ../frontend-service && mvn clean install"
